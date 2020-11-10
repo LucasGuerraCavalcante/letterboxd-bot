1641667e3d57a9fa4cb7dc9734b9fb86bb5f3beb
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-11-10 18:30:58 -0300

    await readFileLinesAndCheckFilms

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 
-const { loginLetterboxd, findFilm, checkView } = require('./services');
+const { loginLetterboxd, readFileLinesAndCheckFilms } = require('./services');
 
 async function letterboxdBot() {
-    // const browser = await puppeteer.launch({ headless: false });
+    // const browser = await puppeteer.launch({ headless: true });
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     
     await page.goto('https://letterboxd.com');
 
+    // Login
     try { await loginLetterboxd(page) } catch (error) {
         console.log("Something went wrong during login: ", error)
     }
 
+    // Using page cookies
     const cookies = await page.cookies();
     await page.setCookie(...cookies);
 
-    try { await findFilm(page) } catch (error) {
-        console.log("Something went wrong while searching for the movie: ", error)
-    }
+    // Reading films file
+    const data = fs.readFileSync('films.txt', 'UTF-8');
+    const lines = data.split(/\r?\n/);
 
-    try { await checkView(page) } catch (error) {
-        console.log("Something went wrong while checking as watched: ", error)
-    }
-  
+    // Reading line by line and checking the films
+    await readFileLinesAndCheckFilms(page, lines)
+
     await browser.close();
 };
 
