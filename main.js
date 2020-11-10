3d7aded829039a8d99e6aebda652a9da87d560f6
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-11-10 15:57:10 -0300

    catch errors

--- main.js
+++ main.js
@@ -1,40 +1,6 @@
 const puppeteer = require('puppeteer');
-const readlineSync = require('readline-sync');
 
-async function loginLetterboxd(page) {
-    await page.waitForSelector('.sign-in-menu');
-
-    const login = readlineSync.question('Username or Email: ');
-    const senha = readlineSync.question('Password: ');
-
-    await page.click('.sign-in-menu');
-    await page.type('input[type="email"]', login);
-    await page.type('input[type="password"]', senha);
-
-    await page.click('input[type="submit"]');
-    await page.waitForNavigation();
-}
-
-async function findFilm(page) {
-    await page.waitForSelector('.js-nav-search-toggle');
-
-    const film = readlineSync.question('Search: ');
-    
-    await page.click('.js-nav-search-toggle');
-    await page.waitForSelector('#search-q');
-    await page.type('#search-q', film);
-
-    await page.keyboard.press('Enter'); 
-
-    await page.waitForSelector('.film-detail-content');
-    await page.click('.film-title-wrapper');
-}
-
-async function checkView(page) {
-    await page.waitForSelector('.-watch');
-    await page.click('.-watch');
-    
-}
+const { loginLetterboxd, findFilm, checkView } = require('./services');
 
 async function letterboxdBot() {
     // const browser = await puppeteer.launch({ headless: false });
@@ -43,14 +9,20 @@ async function letterboxdBot() {
     
     await page.goto('https://letterboxd.com');
 
-    await loginLetterboxd(page)
+    try { await loginLetterboxd(page) } catch (error) {
+        console.log("Something went wrong during login: ", error)
+    }
 
     const cookies = await page.cookies();
     await page.setCookie(...cookies);
 
-    await findFilm(page)
+    try { await findFilm(page) } catch (error) {
+        console.log("Something went wrong while searching for the movie: ", error)
+    }
 
-    await checkView(page)
+    try { await checkView(page) } catch (error) {
+        console.log("Something went wrong while checking as watched: ", error)
+    }
   
     await browser.close();
 };
