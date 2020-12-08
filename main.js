2f31208fd327ad70d4d49a9a0a6da9a78a9f4190
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-12-08 16:24:03 -0300

    catch errors and console logs

--- main.js
+++ main.js
@@ -5,20 +5,49 @@ const { loginLetterboxd, readFileLinesAndCheckFilms } = require('./services');
 
 async function letterboxdBot() {
     // const browser = await puppeteer.launch({ headless: true });
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+
+    // ---------------------------------------------------------------------------------------------
+    console.log("Opening browser...")
+
+    const browser = await puppeteer.launch({ 
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ], 
+        ignoreHTTPSErrors: true,
+        headless: false 
+    })
+        .catch(error => {
+            console.log("Something went wrong during puppeteer.launch: ", error)
+        });
+
+    const [ page ] = await browser.pages()
+        .catch(error => {
+            console.log("Something went wrong during browser.pages: ", error)
+        });
+
+    // ---------------------------------------------------------------------------------------------
+    console.log("Opening Letterboxd...", "\n")
     
-    await page.goto('https://letterboxd.com');
+    await page.goto('https://letterboxd.com', {"waitUntil" : ['load', 'networkidle0', 'domcontentloaded']})
+        .catch(error => {
+            console.log("Something went wrong during page.goto letterboxd: ", error)
+        });
+
+    // ---------------------------------------------------------------------------------------------
+    console.log("Sign in")
 
     // Login
-    try { await loginLetterboxd(page) } catch (error) {
-        console.log("Something went wrong during login: ", error)
-    }
+    await loginLetterboxd(page)
+        .catch(error => {
+            console.log("Something went wrong during login: ", error)
+        });
 
     // Using page cookies
     const cookies = await page.cookies();
     await page.setCookie(...cookies);
 
+    // ---------------------------------------------------------------------------------------------
     // Reading films file
     const data = fs.readFileSync('films.txt', 'UTF-8');
     const lines = data.split(/\r?\n/);
@@ -26,6 +55,7 @@ async function letterboxdBot() {
     // Reading line by line and checking the films
     await readFileLinesAndCheckFilms(page, lines)
 
+    // ---------------------------------------------------------------------------------------------
     await browser.close();
 };
 
--- services/index.js
+++ services/index.js
@@ -5,6 +5,7 @@ async function loginLetterboxd(page) {
 
     const login = readlineSync.question('Username or Email: ');
     const senha = readlineSync.question('Password: ', { hideEchoBack: true });
+    console.log('\n')
 
     try {
         await page.click('.sign-in-menu');
@@ -12,6 +13,7 @@ async function loginLetterboxd(page) {
         await page.type('input[type="password"]', senha);
     
         await page.click('input[type="submit"]');
+
         await page.waitForNavigation();
     }
     catch (error) {
@@ -27,6 +29,7 @@ async function findFilm(page, film) {
     // const film = readlineSync.question('Search: ');
 
     // Film search
+    console.log(`Searching for: ${film}`);
     
     await page.click('.js-nav-search-toggle');
     await page.waitForSelector('#search-q');
@@ -35,12 +38,13 @@ async function findFilm(page, film) {
     await page.keyboard.press('Enter'); 
 
     // Selecting the first result
-
     await page.waitForSelector('.film-title-wrapper');
     await page.click('.film-title-wrapper');
 }
 
 async function checkView(page) {
+    console.log("Checking film as watched...");
+
     await page.waitForSelector('.-watch');
     await page.click('.-watch');
     
@@ -49,13 +53,22 @@ async function checkView(page) {
 async function readFileLinesAndCheckFilms(page, lines) {
     try {
         for (const film of lines) {
-            try { await findFilm(page, film) } catch (error) {
-                console.log("Something went wrong while searching for the movie: ", error);
+            try { 
+                await findFilm(page, film) 
+            } 
+            catch (error) {
+                console.log(`Something went wrong while searching for ${film}: `, error);
             }
         
-            try { await checkView(page) } catch (error) {
-                console.log("Something went wrong while checking as watched: ", error);
-            }           
+            try { 
+                await checkView(page) 
+            } 
+            catch (error) {
+                console.log(`Something went wrong while checking ${film} as watched: `, error);
+            } 
+            finally {
+                console.log(`${film} was checked as watched :)`, "\n");
+            }          
         }
     } catch (error) {
         console.log("Something went wrong while reading the file: ", error);
